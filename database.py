b8ff7e00da3057aeb89d61a6eba3c8eec1993fae
Garvit Kalonia <kaloniagarvit@gmail.com>
2026-09-17 22:39:56 +0530

    update

--- database.py
+++ database.py
@@ -4,6 +4,6 @@ client = MongoClient("mongodb://localhost:27017/")
 
 db = client["d2c_agent"]
 
+customers_collection = db["customers"]
 products_collection = db["products"]
 orders_collection = db["orders"]
-
